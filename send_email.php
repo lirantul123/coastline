30fe2cd6b13e20d6ecd852abd15df71f542d8a76
lirantul123 <93045864+lirantul123@users.noreply.github.com>
2023-07-08 15:35:53 +0300

    e

--- send_email.php
+++ send_email.php
@@ -1,39 +1,20 @@
 <?php
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
+    $to = 'lira.tulchin@gmail.com';
+    $subject = 'New Contact Form Submission';
+    $content = "Name: $name\nEmail: $email\nMessage: $message";
 
-// Get form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-// Create a new PHPMailer instance
-$mail = new PHPMailer();
-
-// SMTP configuration
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'your-email@gmail.com';
-$mail->Password = 'your-email-password';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-// Set sender and recipient
-$mail->setFrom($email, $name);
-$mail->addAddress('lira.tulchin@gmail.com', 'Lira Tulchin');
-
-// Set email content
-$mail->isHTML(false);
-$mail->Subject = 'New Contact Form Submission';
-$mail->Body = "Name: $name\nEmail: $email\nMessage: $message";
-
-// Send the email
-if ($mail->send()) {
-    echo 'Email sent successfully!';
-} else {
-    echo 'Email could not be sent.';
+    if (mail($to, $subject, $content, $headers)) {
+        echo 'Email sent successfully!';
+    } else {
+        echo 'Email could not be sent.';
+    }
 }
+?>
